de3c3287ad85f2ba2d4caf0dda0cd6354e4524f6
conky <bogdan@devbones.com>
2021-09-21 14:13:57 +0300

    YMLA-88: Changed description for step 2. Removed best value label for step 3.

---
+++ modules/openy_memberships_front/app/dist/precache-manifest.c4c3f4993e958a61552ce8f23a89c4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe95a3ac5030a265282",
+    "url": "/assets/css/chunk-vendors.css"
+  },
+  {
+    "revision": "615dfee51a01d7133637",
+    "url": "/assets/js/app.js"
+  },
+  {
+    "revision": "cbe95a3ac5030a265282",
+    "url": "/assets/js/chunk-vendors.js"
+  },
+  {
+    "revision": "02c09ca951ff4b31d192d64093ac07cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d91a158721fc15dd1ed4e2dfe58e865",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- modules/openy_memberships_front/app/dist/service-worker.js
+++ modules/openy_memberships_front/app/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03a77041b795960ee6c5e0c2ccd681ca.js"
+  "/precache-manifest.c4c3f4993e958a61552ce8f23a89c4a7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "openy_memberships"});
